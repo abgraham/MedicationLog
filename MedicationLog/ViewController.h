f8528f96c88b1fa30570c3a3360a4e936076ced8
Annie Graham <abgraham@stanford.edu>
2016-12-29 12:11:21 -0800

    Adds color view to home view controller

---
+++ MedicationLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MedicationLog
+//
+//  Created by Annie Graham on 12/29/16.
+//  Copyright © 2016 Annie Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
